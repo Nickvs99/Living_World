50088f3167c9f24cb96a8733f3f8680feb067ca1
Nickvs99 <vansantennick@gmail.com>
2019-11-09 22:16:02 +0100

    Started with a new scene

--- Living_World/Assets/Scripts/Water.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Water : MonoBehaviour {
-    // Water class
-
-    // TODO give it some speed, or color etc.
-}
