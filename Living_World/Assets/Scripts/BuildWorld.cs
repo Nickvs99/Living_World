f712938a85c269e116c227771728f87be6fd5d10
Nickvs99 <vansantennick@gmail.com>
2019-11-24 00:16:35 +0100

    Inititalize terrain

---
+++ Living_World/Assets/Scripts/BuildWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Creates a procedurally generated world.
+/// </summary>
+public class BuildWorld : MonoBehaviour
+{
+    private GameObject terrainObj;
+    private TerrainData terrainData;
+    private Terrain terrainMain;
+
+    void Start()
+    {
+        CreateTerrain();
+    }
+
+    /// <summary>
+    /// Creates the terrain for the world.
+    /// </summary>
+    private void CreateTerrain() 
+    {
+        Debug.Log("Create terrain");
+
+        terrainData = new TerrainData();
+
+        terrainObj = Terrain.CreateTerrainGameObject(terrainData);
+        terrainMain = terrainObj.GetComponent<Terrain>();
+    }
+}
