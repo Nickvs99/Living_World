6498519aa019949d7ff70d1714e3b1fd32a4dd0c
Nickvs99 <vansantennick@gmail.com>
2019-11-24 17:49:51 +0100

    Raise terrain
    Raise terrain to allow for holes, rivers etc.

--- Living_World/Assets/Scripts/BuildWorld.cs
+++ Living_World/Assets/Scripts/BuildWorld.cs
@@ -25,7 +25,27 @@ public class BuildWorld : MonoBehaviour
 
         terrainData = new TerrainData();
 
+        float maxWidth = 100;
+        float maxLength = 150;
+        float maxHeight = 25;
+
+        terrainData.size = new Vector3(maxWidth, maxHeight, maxLength);
+
         terrainObj = Terrain.CreateTerrainGameObject(terrainData);
         terrainMain = terrainObj.GetComponent<Terrain>();
+
+        int heightmapWidth = terrainData.heightmapWidth;
+        int heightmapHeight = terrainData.heightmapHeight;
+        
+        float[,] heights = terrainData.GetHeights(0, 0, heightmapWidth, heightmapHeight);
+
+        // Raise terrain to allow for holes, rivers etc.
+        for (int i = 0; i < heightmapWidth; i++) {
+            for (int j = 0; j<  heightmapHeight; j++) {
+                heights[i, j] = 5 / maxHeight;
+            }
+        }
+
+        terrainData.SetHeights(0, 0, heights);
     }
 }
