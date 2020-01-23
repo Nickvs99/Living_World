1b0e584e36ec7b0118c121bc1c12d02dff07b3f7
Nickvs99 <vansantennick@gmail.com>
2020-01-23 22:28:59 +0100

    Vegetation objects can't overlap anymore

--- Living_World/Assets/Scripts/TerrainGenerator.cs
+++ Living_World/Assets/Scripts/TerrainGenerator.cs
@@ -35,7 +35,6 @@ public class TerrainGenerator : MonoBehaviour {
     /// </summary>
     private void GenerateTerrain() {
 
-        
         Debug.Log("Create terrain");
 
         Mesh mesh = new Mesh();
@@ -95,14 +94,17 @@ public class TerrainGenerator : MonoBehaviour {
     }
     
     /// <summary>
-    /// 
+    /// Adds vegetation tot the terrain.
     /// </summary>
     private void AddVegetation() {
 
         Debug.Log("Adding vegetation");
 
+        // Resolution of vegetationMap
+        int resolution = 10;
+        
         // This map stores the density of vegetation
-        float[,] vegetationMap = new float[xSize / 10, zSize / 10];
+        float[,] vegetationMap = new float[xSize / resolution, zSize / resolution];
 
         // Get a random vegetation map. In the future this will depend on objects in the vicinity.
         for (int i = 0; i < vegetationMap.GetLength(0); i++) {
@@ -111,78 +113,99 @@ public class TerrainGenerator : MonoBehaviour {
             }
         }
 
-        for (int i = 0; i < vegetationMap.GetLength(0); i++) {
-            for (int j = 0; j < vegetationMap.GetLength(1); j++) {
-                
-                List<GameObject> trees = new List<GameObject>();
-                // Add trees
-                for (int k = 0; k < 10; k++) {
+        // Create empty vegetation object for organizing objects
+        GameObject vegetation = new GameObject();
+        vegetation.transform.parent = gameObject.transform;
+        vegetation.name = "Vegetation";
 
+        AddVegetationObjects(tree, vegetation, 10, vegetationMap);
+        AddVegetationObjects(grass, vegetation, 25, vegetationMap);
+                
+    }
+
+    /// <summary>
+    /// Adds objects at a random place on the map. The vegetationMap desribes the density of objects per "tile"
+    /// </summary>
+    /// <param name="obj">GameObject: Objects which are instantiated</param>
+    /// <param name="parent">GameObject: Parent of the instantiated object</param>
+    /// <param name="maxPopulation">int: The maximum population for each section of the vegetationmap</param>
+    /// <param name="vegetationMap">float[,]: The vegetationMap, describes the vegetation density of the terrain.</param>
+    private void AddVegetationObjects(GameObject obj, GameObject parent, int maxPopulation, float[,] vegetationMap){
+        
+        int resolution = xSize / vegetationMap.GetLength(0);
+
+        // Creates a folder object for all instantiated objects
+        GameObject folder = new GameObject();
+        folder.transform.parent = parent.transform;
+        folder.name = obj.name + "s";
+
+        // All newly added objects
+        List<GameObject> objects = new List<GameObject>();
+        
+        // Loop over all sections of the vegetationMap
+        for(int i = 0; i < vegetationMap.GetLength(0); i++){
+            for(int j = 0; j < vegetationMap.GetLength(1); j++){
+
+                // Try to add all objects
+                for (int k = 0; k < maxPopulation; k++) {
+                    
+                    // Check if it is allowed based on the vegetationMap
                     float r = Random.Range(0f, 1f);
                     if (r > vegetationMap[i, j]) {
                         continue;
                     }
-
-                    // float xCor = Random.Range(i * 10f, (i + 1) * 10f);
-                    // float zCor = Random.Range(j * 10f, (j + 1) * 10f);
-                    // float yCor = GetHeightTerrain(xCor, zCor);
-                    // Instantiate(tree, new Vector3(xCor, yCor, zCor), Quaternion.identity);
 
                     int while_count = 0;
                     while(while_count < 100){
-                        float xCor = Random.Range(i * 10f, (i + 1) * 10f);
-                        float zCor = Random.Range(j * 10f, (j + 1) * 10f);
+
+                        // Pick a random x and z coordinate within the section
+                        float xCor = Random.Range((float)i * resolution, (i + 1) * resolution);
+                        float zCor = Random.Range((float)j * resolution, (j + 1) * resolution);
                         float yCor = GetHeightTerrain(xCor, zCor);
 
+                        // Check if there is enough room for the object
                         bool valid = true;
-                        foreach(GameObject treeObj in trees){
+                        foreach(GameObject treeObj in objects){
 
-                            // TODO dist 1 should depend on the tree, it shouldn't be a magic number
+                            // TODO dist 1 should depend on the obj, it shouldn't be a magic number
                             if (Utility.dist(treeObj.transform.position, new Vector3(xCor, yCor, zCor)) < 1){
                                 valid = false;
-                                Debug.Log("Not Valid");
-                                Debug.Log($"{xCor}, {zCor}");
-                                Debug.Log($"{treeObj.transform.position}");
+                                break;
+
                             }
                         }
-                        if (valid){
-                            GameObject newTree = Instantiate(tree, new Vector3(xCor, yCor, zCor), Quaternion.identity);
-                            trees.Add(newTree);
-                            break;
-
+                        if (!valid){
+                            while_count += 1;
+                            continue;
                         }
-                        while_count += 1;
-                    }
-                    
-                }
-                
-                // Add grass
-                for( int k = 0; k < 25; k++) {
 
-                    float r = Random.Range(0f, 1f);
-                    if (r > vegetationMap[i, j]) {
-                        continue;
+                        GameObject newObj = Instantiate(obj, new Vector3(xCor, yCor, zCor), Quaternion.identity);
+                        newObj.transform.parent = folder.transform;
+                        newObj.name = obj.name + "_" + objects.Count;
+                        objects.Add(newObj);
+                        break;       
                     }
 
-                    float xCor = Random.Range(i * 10, (i + 1) * 10);
-                    float zCor = Random.Range(j * 10, (j + 1) * 10);
-                    float yCor = GetHeightTerrain(xCor, zCor);
-
-                    Instantiate(grass, new Vector3(xCor, yCor, zCor), Quaternion.identity);
+                    if (while_count == 100){
+                        Debug.LogWarning("Placement of {obj} went wrong. No room found!");
+                        return;
+                    }
                 }
             }
         }
     }
 
     /// <summary>
-    /// 
+    /// Get the height of the terrain at a x and z coordinate.
     /// </summary>
-    /// <param name="x"></param>
-    /// <param name="z"></param>
+    /// <param name="x">float: x coordinate</param>
+    /// <param name="z">float: z coordinate</param>
     /// <returns></returns>
-    private float GetHeightTerrain(float x, float z){
-
+    public float GetHeightTerrain(float x, float z){
+        
+        // TODO shouldm't be a magic number, now the terrain would be limited to 100 meters
         float maxHeight = 100f;
+
         Ray ray = new Ray(new Vector3(x,maxHeight,z), Vector3.down);
         if (GetComponent<MeshCollider>().Raycast(ray, out RaycastHit hit, maxHeight)) {
             return hit.point.y;
--- Living_World/Assets/Scripts/Utility.cs
+++ Living_World/Assets/Scripts/Utility.cs
@@ -20,6 +20,12 @@ public static class Utility {
     }
 
 
+    /// <summary>
+    /// Returns the distance between 2 vectors
+    /// </summary>
+    /// <param name="v1">Vector3</param>
+    /// <param name="v2">Vector3</param>
+    /// <returns>float: Distance between the two vectors</returns>
     public static float dist(Vector3 v1, Vector3 v2){
 
         return Mathf.Pow(Mathf.Pow(v1.x - v2.x, 2) + Mathf.Pow(v1.y - v2.y, 2) + Mathf.Pow(v1.z - v2.z, 2), 0.5f);
